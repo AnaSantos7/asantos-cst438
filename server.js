7b89c37b1f39c710d1b7a0c66f625e165170620d
AnaSantos7 <asantos@csumb.edu>
2019-09-09 18:47:43 -0700

    updated server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/project'));
+app.use(express.static(__dirname + '/dist/asantos-cst438'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/project/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/asantos-cst438/index.html'));
 });
 
 // default Heroku PORT
